64baa15022183933a923a002b82b5703077a0bad
nmalhotra2024 <157635139+nmalhotra2024@users.noreply.github.com>
2025-07-05 13:24:43 +0200

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,37 @@
+#Activity one
+#Print a Sentence
+print("Welcome to the world of programming")
+
+#Print a number
+print(4)
+
+#Next Line after print
+print("Hello python \n")
+
+#Pass multiple arguments for printing
+print("Akshit",6)
+
+#End argument of Print Statement
+print("Welcome to", end= "*")
+
+#Activity tow
+# Creating diffrent variables
+x = 5 
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User input
+name = input("enter your name:")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+#Activity three
+import keyword
+
+#Print all the python keywords
+print("Python keywords are: ")
+print(keyword.kwlist)
